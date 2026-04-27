221186a5b98356e62e6b12eb2df71a01039bb953
Tynkozaurus <140120590+Tynkozaurus@users.noreply.github.com>
2026-04-27 19:54:23 +0200

    Update bigM.js
    pardon

--- bigM.js
+++ bigM.js
@@ -1,26 +1,48 @@
-// .frequency-position__one-item.nieobecnosc{
-//     border-top-color: #2f7d42;
-//     border-bottom-color: #2f7d42;
-//     background-color: #f7f8fb;
-// }
+// ==UserScript==
+// @name        New script
+// @namespace   Violentmonkey Scripts
+// @match       *://uczen.eduvulcan.pl/*
+// @grant       none
+// @version     1.0
+// ==/UserScript==
 
+const run = () => {
 
-const elements = document.querySelectorAll('.frequency-position__one-item.nieobecnosc');
-
-elements.forEach(el => {
- el.style.borderTopColor = '#2f7d42';
-el.style.borderBottomColor = '#2f7d42';
-el.style.backgroundColor = '#f7f8fb';
+    document.querySelectorAll('.column').forEach(div => {
+  if (div.textContent.includes('27') &&    div.textContent.includes('poniedziałek')) {
+    div.classList.add("tenDiv");
+  }
 });
 
-const elements2 = document.querySelectorAll('.frekwencja-symbol.nieobecnosc');
 
-elements2.forEach(el => {
 
-if(el.closest('.frekwencja-symbol-kontener')){
-el.style.backgroundColor = '#2f7d42';
-el.innerHTML = "O";
+
+
+  const elements = document.querySelectorAll('.frequency-position__one-item.nieobecnosc');
+
+  console.log("elements:", elements.length);
+
+  elements.forEach(el => {
+
+if(el.closest('.tenDiv')){
+
+
+    el.style.borderTopColor = '#2f7d42';
+    el.style.borderBottomColor = '#2f7d42';
+    el.style.backgroundColor = '#f7f8fb';
 }
 
-});
 
+  });
+
+  const elements2 = document.querySelectorAll('.frekwencja-symbol.nieobecnosc');
+
+  elements2.forEach(el => {
+    if (el.closest('.frekwencja-symbol-kontener')   && el.closest('.tenDiv')) {
+      el.style.backgroundColor = '#2f7d42';
+      el.innerHTML = "O";
+    }
+  });
+};
+
+setInterval(run, 100);
